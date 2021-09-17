48e9391a544dc253488943f2cb16bdd5e4c0fe2e
Neiloy Hassan <neiloyhassan1@gmail.com>
2021-09-17 23:32:30 +0600

    google sign in initialized

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:quidtrails/controller/auth.dart';
 import 'package:quidtrails/controller/network.dart';
 
